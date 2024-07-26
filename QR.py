4d02ff7b52d77b2bde4d332a7f22b0d1d3653afd
Aayush Paudel <152758729+aayushpaudel07@users.noreply.github.com>
2024-07-26 15:55:18 +0545

    Update QR.py

--- QR.py
+++ QR.py
@@ -7,5 +7,5 @@ qr=qrcode.QRCode(version=1,box_size=10,border=5,error_correction=qrcode.constant
 qr.add_data(url)
 qr.make(fit=True)
 img=qr.make_image(fill_color='black',back_color='white')
-img.save('C:/Users/patel/Desktop/qr.png')
+img.save('C:/Users/"GIVW YOUR USERNAME HERE"/Desktop/qr.png') //create your own path you want to save
 
